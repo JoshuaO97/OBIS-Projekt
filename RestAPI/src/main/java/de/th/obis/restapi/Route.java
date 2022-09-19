22fe6deda3ab9eadccd06e80c620364a20b57684
JoshuaO97 <85791900+JoshuaO97@users.noreply.github.com>
2022-09-19 15:40:55 +0200

    change var altitude to ascent and descent in Route.java and add 3 routes to RouteController.java

--- RestAPI/src/main/java/de/th/obis/restapi/Route.java
+++ RestAPI/src/main/java/de/th/obis/restapi/Route.java
@@ -7,18 +7,20 @@ public class Route {
 
     private final int id;
     private static int nextID = 1;
-    private String name;
+    private final String name;
     private final double length;
-    private final double altitude;
+    private final int ascent;
+    private final int descent;
     private final int numCaches;
     private final ArrayList<Point> caches;
 
 
-    public Route(String name, double length, double altitude, ArrayList<Point> caches) {
+    public Route(String name, double length, int ascent, int descent, ArrayList<Point> caches) {
         this.id = nextID++;
         this.name = name;
         this.length = length;
-        this.altitude = altitude;
+        this.ascent = ascent;
+        this.descent = descent;
         this.numCaches = caches.size();
         this.caches = caches;
     }
@@ -32,16 +34,16 @@ public class Route {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getLength() {
         return length;
     }
 
-    public double getAltitude() {
-        return altitude;
+    public double getAscent() {
+        return ascent;
+    }
+
+    public double getDescent() {
+        return descent;
     }
 
     public int getNumCaches() {
--- RestAPI/src/main/java/de/th/obis/restapi/RouteController.java
+++ RestAPI/src/main/java/de/th/obis/restapi/RouteController.java
@@ -1,13 +1,19 @@
 package de.th.obis.restapi;
 
+import de.th.obis.restapi.point.Point;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/routes")
 public class RouteController {
 
-    private ArrayList<Route> routes = new ArrayList<>();
+    private final Route route1 = new Route("Burg Hunolstein", 1764, 79, 82, new ArrayList<>(Arrays.asList(new Point(49.79902337229739, 7.045904398096021), new Point(49.80374204785129, 7.043684365510524), new Point(49.81101479063034, 7.047369853566575))));
+    private final Route route2 = new Route("Eremitenpfad", 1121, 3, 79, new ArrayList<>(Arrays.asList(new Point(49.88725337891083, 7.870928926186527), new Point(49.88544048334817, 7.879936540611833))));
+    private final Route route3 = new Route("Sonnenberghuette", 1981, 55, 18, new ArrayList<>(Arrays.asList(new Point(49.897002220658976, 7.842485473574225), new Point(49.895138684854146, 7.863365895921416))));
+
+    private ArrayList<Route> routes = new ArrayList<>(Arrays.asList(route1, route2, route3));
 
     @GetMapping
     public ArrayList<Route> getAllRoutes() {
@@ -21,5 +27,4 @@ public class RouteController {
         }
         return routes.get(nr - 1);
     }
-
 }
